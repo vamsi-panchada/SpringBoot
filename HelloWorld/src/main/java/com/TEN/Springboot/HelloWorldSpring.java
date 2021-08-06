e344f2ee5ffdefa408d91cf559bd45da3fd9710c
vamsi panchada <62779852+vamsi-panchada@users.noreply.github.com>
2021-08-06 22:04:29 +0530

    Add files via upload

---
+++ HelloWorld/src/main/java/com/TEN/Springboot/HelloWorldSpring.java
@@ -0,0 +1,11 @@
+package com.TEN.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpring {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSpring.class, args);
+    }
+}
