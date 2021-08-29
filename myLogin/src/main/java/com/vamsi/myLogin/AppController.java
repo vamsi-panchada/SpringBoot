2afa3d72836a12c3995beaea84387bfbe0821343
vamsi panchada <62779852+vamsi-panchada@users.noreply.github.com>
2021-08-29 22:12:20 +0530

    Add files via upload

---
+++ myLogin/src/main/java/com/vamsi/myLogin/AppController.java
@@ -0,0 +1,44 @@
+package com.vamsi.myLogin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String  showSignUpForm(Model model){
+        model.addAttribute("user", new User());
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegisteration(User user){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "register_success";
+    }
+
+    @GetMapping("/list_users")
+    public String viewUsersList(Model model){
+        List<User> usersList = userRepository.findAll();
+        model.addAttribute("user_list", usersList);
+        return "users";
+    }
+
+}
---
+++ myLogin/src/test/java/com/vamsi/myLogin/MyLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamsi.myLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
